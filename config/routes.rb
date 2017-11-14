bd79774ed457b328abfbfea60e967088d2734408
Yonatan <yvalenta@gmail.com>
2017-11-14 10:56:01 -0500

    - [X] Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :links
   resources :categories
+  resources :users
   devise_for :users
-	root 'categories#index'
+  root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
